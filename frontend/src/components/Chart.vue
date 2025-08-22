4d62ad446b64ac6e34bc348f2bfa7d9d0ef069ef
mykhaylo <mshchygol@gmail.com>
2025-08-22 12:30:51 +0300

    chart style updates

--- frontend/src/components/Chart.vue
+++ frontend/src/components/Chart.vue
@@ -17,84 +17,87 @@ watch(() => props.data, (newValue, oldValue) => {
 });
 
 function drawChart() {
-  const margin = { top: 20, right: 30, bottom: 40, left: 40 };
-  width = 800 - margin.left - margin.right;
-  height = 400 - margin.top - margin.bottom;
+    const margin = { top: 20, right: 30, bottom: 40, left: 40 };
+        width = 1200 - margin.left - margin.right;
+        height = 400 - margin.top - margin.bottom;
 
-  svg = d3
-    .select(chartContainer.value)
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
+    svg = d3
+        .select(chartContainer.value)
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
 
-  // scales
-  x = d3
-    .scaleBand()
-    .domain(props.data.map((d) => d.name))
-    .range([0, width])
-    .padding(0.1);
+    // scales
+    x = d3.scaleLinear()
+        .domain(d3.extent(props.data, d => +d.name)) // min → max price
+        .range([0, width]);
 
-  y = d3
-    .scaleLinear()
-    .domain([0, d3.max(props.data, (d) => d.value)])
-    .nice()
-    .range([height, 0]);
+    y = d3
+        .scaleLinear()
+        .domain([0, d3.max(props.data, (d) => d.value)])
+        .nice()
+        .range([height, 0]);
 
-  // bars group
-  svg.append("g").attr("class", "bars");
+    // bars group
+    svg.append("g").attr("class", "bars");
 
-  // axes
-  svg.append("g").attr("class", "x-axis").attr("transform", `translate(0,${height})`).call(d3.axisBottom(x));
-  svg.append("g").attr("class", "y-axis").call(d3.axisLeft(y));
+    // axes
+    svg.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(x).ticks(10).tickFormat(d3.format(".2f"))); 
+    svg.append("g").attr("class", "y-axis").call(d3.axisLeft(y));
 
-  // draw initial bars
-  updateChart();
+    // draw initial bars
+    updateChart();
 }
 
 function updateChart() {
-  // update scales
-  x.domain(props.data.map((d) => d.name));
-  y.domain([0, d3.max(props.data, (d) => d.value)]).nice();
+    // update scales
+    x.domain(d3.extent(props.data, d => +d.name));
+    y.domain([0, d3.max(props.data, (d) => d.value)]).nice();
 
-  const bars = svg
-    .select(".bars")
-    .selectAll(".bar")
-    .data(props.data, (d) => d.name);
+    const bars = svg
+        .select(".bars")
+        .selectAll(".bar")
+        .data(props.data, (d) => d.name);
 
-  // ENTER
-  bars
-    .enter()
-    .append("rect")
-    .attr("class", "bar")
-    .attr("x", (d) => x(d.name))
-    .attr("width", x.bandwidth())
-    .attr("fill", "steelblue")
-    .attr("y", y(0))
-    .attr("height", 0)
-    .merge(bars) // ENTER + UPDATE
-    .transition()
-    .duration(500)
-    .attr("x", (d) => x(d.name))
-    .attr("y", (d) => y(d.value))
-    .attr("width", x.bandwidth())
-    .attr("height", (d) => height - y(d.value));
+    const midIndex = Math.floor(props.data.length / 2);
+    const barWidth = Math.max(1, width / props.data.length);
+    // ENTER
+    bars
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", (d) => x(d.name))
+        .attr("width", barWidth)
+        .attr("fill", (d, i) => (i < midIndex ? "green" : "red"))
+        .attr("y", y(0))
+        .attr("height", 0)
+        .merge(bars) // ENTER + UPDATE
+        .transition()
+        .duration(200)
+        .attr("x", (d) => x(d.name))
+        .attr("y", (d) => y(d.value))
+        .attr("width", barWidth)
+        .attr("height", (d) => height - y(d.value))
+        .attr("fill", (d, i) => (i < midIndex ? "green" : "red")); // update fill too
 
-  // EXIT
-  bars.exit().remove();
+    // EXIT
+    bars.exit().remove();
 
-  // update axes
-  svg.select(".x-axis").transition().duration(200).call(d3.axisBottom(x));
-  svg.select(".y-axis").transition().duration(200).call(d3.axisLeft(y));
+    // update axes
+    svg.select(".x-axis").transition().duration(200).call(d3.axisBottom(x));
+    svg.select(".y-axis").transition().duration(200).call(d3.axisLeft(y));
 }
+
 
 </script>
 
 <template>
-  <div ref="chartContainer" class="w-full h-[400px]"></div>
-  <div>{{ props.message?.data?.timestamp }}</div>
-  <!-- <div>{{ data }}</div> -->
+    <div ref="chartContainer" class="w-full h-[400px]"></div>
 </template>
 
 <style scoped>
